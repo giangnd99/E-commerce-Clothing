906e7e8eecfad88aaf93f755fa69a5aa14e3f392
giangnd99 <nguyendanggiang99@gmail.com>
2025-02-25 05:00:06 +0700

    update

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/secondary/CartItem2Repository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.asm.fpoly_asm_springboot.repositories.secondary;
-
-import com.springboot.asm.fpoly_asm_springboot.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItem2Repository extends JpaRepository<CartItem, Integer> {
-}
--- src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/secondary/ProductOrder2Repository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.asm.fpoly_asm_springboot.repositories.secondary;
-
-import com.springboot.asm.fpoly_asm_springboot.entity.ProductOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductOrder2Repository extends JpaRepository<ProductOrder, Integer> {
-}
